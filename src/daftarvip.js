f2674418af4529dd9fcaa1d6dbf5beca1d9aad6b
LIPEx1738 <fellipesiqueira812@gmail.com>
2021-08-08 23:40:45 -0300

    First commit

---
+++ src/daftarvip.js
@@ -0,0 +1,15 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DO PREMIUM:*
+
+-1. 10 > Acessar recursos premium
+-2. 40 > Recursos vip + base do bot editável!
+
+*SE QUER TER PREMIUM:*
+
+*CHAME O LIPEXX:*
+
+_wa.me/5511993943794ou digite *${prefix}owner*_ `
+}
+exports.daftarvip = daftarvip
